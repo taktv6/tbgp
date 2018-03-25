5f1d4c613783d043ffa67969eec1ad869c737191
Oliver Herms <oliver.herms@exaring.de>
2018-03-25 17:09:27 +0200

    Adding tests for LPM

--- lpm/lpm_test.go
+++ lpm/lpm_test.go
@@ -21,46 +21,48 @@ func TestInsert(t *testing.T) {
 	tests := []struct {
 		name     string
 		prefixes []*net.Prefix
-		wantFail bool
-		//expected uint32
+		expected *node
 	}{
 		{
 			name: "Insert first node",
 			prefixes: []*net.Prefix{
 				net.NewPfx(167772160, 8), // 10.0.0.0/8
-				net.NewPfx(0, 0),         // 0.0.0.0/0
 			},
-			wantFail: false,
-			//expected: 100,
+			expected: &node{
+				pfx:  net.NewPfx(167772160, 8), // 10.0.0.0/8
+				skip: 8,
+			},
 		},
 		{
-			name: "Insert first node",
+			name: "Insert double node",
 			prefixes: []*net.Prefix{
 				net.NewPfx(167772160, 8), // 10.0.0.0/8
-				net.NewPfx(0, 0),         // 0.0.0.0/0
-
+				net.NewPfx(167772160, 8), // 10.0.0.0/8
+				net.NewPfx(167772160, 8), // 10.0.0.0/8
+				net.NewPfx(167772160, 8), // 10.0.0.0/8
 			},
-			wantFail: false,
-			//expected: 100,
+			expected: &node{
+				pfx:  net.NewPfx(167772160, 8), // 10.0.0.0/8
+				skip: 8,
+			},
 		},
 		{
-			name: "Insert first node",
+			name: "Insert triangle",
 			prefixes: []*net.Prefix{
 				net.NewPfx(167772160, 8), // 10.0.0.0
-				net.NewPfx(134217728, 5), // 8.0.0.0
-				net.NewPfx(268435456, 5), // 16.0.0.0
+				net.NewPfx(167772160, 9), // 10.0.0.0
+				net.NewPfx(176160768, 9), // 10.128.0.0
 			},
-			wantFail: false,
-			//expected: 100,
-		},
-		{
-			name: "Insert first node",
-			prefixes: []*net.Prefix{
-				net.NewPfx(167772160, 8), // 10.0.0.0
-				net.NewPfx(184549376, 8), // 11.0.0.0
+			expected: &node{
+				pfx:  net.NewPfx(167772160, 8), // 10.0.0.0/8
+				skip: 8,
+				l: &node{
+					pfx: net.NewPfx(167772160, 9), // 10.0.0.0
+				},
+				h: &node{
+					pfx: net.NewPfx(176160768, 9), // 10.128.0.0
+				},
 			},
-			wantFail: false,
-			//expected: 100,
 		},
 		{
 			name: "Insert disjunct prefixes",
@@ -68,8 +70,18 @@ func TestInsert(t *testing.T) {
 				net.NewPfx(167772160, 8),  // 10.0.0.0
 				net.NewPfx(191134464, 24), // 11.100.123.0/24
 			},
-			wantFail: false,
-			//expected: 100,
+			expected: &node{
+				pfx:   net.NewPfx(167772160, 7), // 10.0.0.0/7
+				skip:  7,
+				dummy: true,
+				l: &node{
+					pfx: net.NewPfx(167772160, 8), // 10.0.0.0/8
+				},
+				h: &node{
+					pfx:  net.NewPfx(191134464, 24), // 10.0.0.0/8
+					skip: 16,
+				},
+			},
 		},
 		{
 			name: "Insert disjunct prefixes plus one child",
@@ -79,24 +91,36 @@ func TestInsert(t *testing.T) {
 				net.NewPfx(167772160, 12), // 10.0.0.0
 				net.NewPfx(167772160, 10), // 10.0.0.0
 			},
-			wantFail: false,
-			//expected: 100,
+			expected: &node{
+				pfx:   net.NewPfx(167772160, 7), // 10.0.0.0/7
+				skip:  7,
+				dummy: true,
+				l: &node{
+					pfx: net.NewPfx(167772160, 8), // 10.0.0.0/8
+					l: &node{
+						skip: 1,
+						pfx:  net.NewPfx(167772160, 10), // 10.0.0.0/10
+						l: &node{
+							skip: 1,
+							pfx:  net.NewPfx(167772160, 12), // 10.0.0.0
+						},
+					},
+				},
+				h: &node{
+					pfx:  net.NewPfx(191134464, 24), // 10.0.0.0/8
+					skip: 16,
+				},
+			},
 		},
 	}
 
 	for _, test := range tests {
-		fmt.Printf("\nTest: %s\n", test.name)
 		l := New()
 		for _, pfx := range test.prefixes {
 			l.Insert(pfx)
 		}
 
-		l.root.dump()
-		fmt.Printf("\n-----------------------------\n\n")
-
-		//if l.root.key != test.expected {
-		//t.Errorf("Test %s failed: ")
-		//}
+		assert.Equal(t, test.expected, l.root)
 	}
 }
 
@@ -121,6 +145,12 @@ func TestLPM(t *testing.T) {
 				net.NewPfx(167772160, 10), // 10.0.0.0
 				net.NewPfx(167772160, 12), // 10.0.0.0
 			},
+		},
+		{
+			name:     "Test 2",
+			prefixes: []*net.Prefix{},
+			needle:   net.NewPfx(167772160, 32), // 10.0.0.0/32
+			expected: nil,
 		},
 	}
 
@@ -151,6 +181,12 @@ func TestGet(t *testing.T) {
 			needle:   net.NewPfx(167772160, 8), // 10.0.0.0/8
 			expected: net.NewPfx(167772160, 8), // 10.0.0.0/8
 		},
+		{
+			name:     "Test 2",
+			prefixes: []*net.Prefix{},
+			needle:   net.NewPfx(167772160, 32), // 10.0.0.0/32
+			expected: nil,
+		},
 	}
 
 	for _, test := range tests {
@@ -159,11 +195,15 @@ func TestGet(t *testing.T) {
 			lpm.Insert(pfx)
 		}
 		p := lpm.Get(test.needle, false)
-		if p == nil && test.expected != nil {
-			t.Errorf("Test %s: Unexpected nil result: Expected %s\n", test.name, test.expected.String())
+
+		if p == nil {
+			if test.expected != nil {
+				t.Errorf("Unexpected nil result for test %s", test.name)
+			}
+			continue
 		}
 
-		assert.Equal(t, test.expected.String(), p[0].String())
+		assert.Equal(t, test.expected, p[0])
 	}
 }
 
@@ -207,18 +247,6 @@ func TestGetBitUint32(t *testing.T) {
 		offset   uint8
 		expected bool
 	}{
-		/*{
-			name:     "test 1",
-			input:    128,
-			offset:   7,
-			expected: true,
-		},
-		{
-			name:     "test 2",
-			input:    128,
-			offset:   8,
-			expected: false,
-		},*/
 		{
 			name:     "test 3",
 			input:    16777216,
@@ -229,12 +257,104 @@ func TestGetBitUint32(t *testing.T) {
 
 	for _, test := range tests {
 		fmt.Println(strconv.FormatInt(int64(test.input), 2))
-		/*for i := 0; i < 32; i++ {
-			fmt.Printf("%d = %v\n", i, getBitUint32(test.input, uint8(i)))
-		}*/
 		b := getBitUint32(test.input, test.offset)
 		if b != test.expected {
 			t.Errorf("%s: Unexpected failure: Bit %d of %d is %v. Expected %v", test.name, test.offset, test.input, b, test.expected)
 		}
+	}
+}
+
+func TestInsertChildren(t *testing.T) {
+	tests := []struct {
+		name     string
+		base     *net.Prefix
+		old      *net.Prefix
+		new      *net.Prefix
+		expected *node
+	}{
+		{
+			name: "Test 1",
+			base: net.NewPfx(167772160, 8), //10.0.0.0/8
+			old:  net.NewPfx(167772160, 9), //10.0.0.0/9
+			new:  net.NewPfx(176160768, 9), //10.128.0.0/9
+			expected: &node{
+				pfx:   net.NewPfx(167772160, 8),
+				skip:  8,
+				dummy: true,
+				l: &node{
+					pfx: net.NewPfx(167772160, 9),
+				},
+				h: &node{
+					pfx: net.NewPfx(176160768, 9),
+				},
+			},
+		},
+		{
+			name: "Test 2",
+			base: net.NewPfx(167772160, 8), //10.0.0.0/8
+			old:  net.NewPfx(176160768, 9), //10.128.0.0/9
+			new:  net.NewPfx(167772160, 9), //10.0.0.0/9
+			expected: &node{
+				pfx:   net.NewPfx(167772160, 8),
+				skip:  8,
+				dummy: true,
+				l: &node{
+					pfx: net.NewPfx(167772160, 9),
+				},
+				h: &node{
+					pfx: net.NewPfx(176160768, 9),
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		n := newNode(test.base, test.base.Pfxlen(), true)
+		old := newNode(test.old, test.old.Pfxlen(), false)
+		n.insertChildren(old, test.new)
+		assert.Equal(t, test.expected, n)
+	}
+}
+
+func TestInsertBefore(t *testing.T) {
+	tests := []struct {
+		name     string
+		a        *net.Prefix
+		b        *net.Prefix
+		expected *node
+	}{
+		{
+			name: "Test 1",
+			a:    net.NewPfx(167772160, 10), // 10.0.0.0
+			b:    net.NewPfx(167772160, 8),  // 10.0.0.0
+			expected: &node{
+				pfx: net.NewPfx(167772160, 8), // 10.0.0.0,
+				l: &node{
+					pfx:  net.NewPfx(167772160, 10), // 10.0.0.0
+					skip: 1,
+				},
+				skip: 1,
+			},
+		},
+		{
+			name: "Test 2",
+			a:    net.NewPfx(184549376, 8), // 11.0.0.0/8
+			b:    net.NewPfx(167772160, 7), // 10.0.0.0/7
+			expected: &node{
+				pfx: net.NewPfx(167772160, 7), // 10.0.0.0,
+				h: &node{
+					pfx:  net.NewPfx(184549376, 8), // 10.0.0.0
+					skip: 0,
+				},
+				skip: 1,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		n := newNode(test.a, test.a.Pfxlen(), false)
+		fmt.Printf("n.skip: %d\n", n.skip)
+		n = n.insertBefore(test.b, test.b.Pfxlen())
+		assert.Equal(t, test.expected, n)
 	}
 }
